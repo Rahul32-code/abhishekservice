43229346a33ff5c16be44bbfc5b39b7e62d468cc
Rahul32-code <114091763+Rahul32-code@users.noreply.github.com>
2025-08-23 20:19:01 +0530

    contact form

--- src/components/ContactModel.jsx
+++ src/components/ContactModel.jsx
@@ -55,7 +55,7 @@ const ContactModal = ({ isOpen, onClose }) => {
 
         <div className="grid grid-cols-1">
           <div className="p-8">
-            <h2 className="text-3xl font-bold text-indigo-600 mb-2">
+            <h2 className="text-3xl font-bold text-blue-700 mb-2">
               Contact Us
             </h2>
             <p className="text-gray-600 mb-6">We’d love to hear from you!</p>
@@ -82,7 +82,7 @@ const ContactModal = ({ isOpen, onClose }) => {
                       maxLength={name === "phone" ? 10 : undefined}
                       pattern={pattern}
                       placeholder={
-                        name === "phone" ? "Enter 10-digit mobile number" : ""
+                        name === "phone" ? "Enter 10-digit mobile number" : name === "email" ? "info@gmail.com" :"john doe"
                       }
                       className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400"
                       autoComplete="off"
@@ -104,7 +104,7 @@ const ContactModal = ({ isOpen, onClose }) => {
               <button
                 type="submit"
                 disabled={loading}
-                className="w-full py-2 px-4 bg-indigo-600 cursor-pointer hover:bg-indigo-700 text-white font-semibold rounded-md flex items-center justify-center"
+                className="w-full py-2 px-4 bg-blue-700 cursor-pointer hover:bg-blue-900 text-white font-semibold rounded-md flex items-center justify-center"
               >
                 {loading && <Loader2 className="w-5 h-5 mr-2 animate-spin" />}
                 {loading ? "Sending..." : "Send Message"}
