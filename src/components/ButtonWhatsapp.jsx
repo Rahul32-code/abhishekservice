dd458b7201d6cd032ad19f563980b0858f9f9c12
Rahul32-code <114091763+Rahul32-code@users.noreply.github.com>
2025-08-23 20:26:36 +0530

    changes w btn

--- src/components/ButtonWhatsapp.jsx
+++ src/components/ButtonWhatsapp.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import { FaWhatsapp } from 'react-icons/fa'
+import { link } from "framer-motion/client";
+import { FaWhatsapp } from "react-icons/fa";
+
+const links = [
+  {
+    href: "https://wa.me/919266594942?text=Hi%2C%20I%27m%20interested%20in%20your%20services",
+    icon: <FaWhatsapp className="text-6xl w-10 h-10" />,
+  },
+];
 
 const ButtonWhatsApp = () => {
   return (
-    <div className='fixed bottom-5 right-5 z-50 flex items-center justify-center'>
-      <a 
-        href="https://wa.me/11234567890"
-        target='_blank'
-        rel='noopener noreferrer'
-        className=' text-white shadow-lg rounded-xl p-2 bg-green-500 hover:bg-green-600 transition duration-300'
-      >
-        <FaWhatsapp className='text-6xl w-10 h-10' />
-      </a>
+    <div className="fixed bottom-5 right-5 z-50 flex items-center justify-center">
+      {links.map((link, index) => {
+        return (
+          <a
+            key={index}
+            href={link.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" text-white shadow-lg rounded-xl p-2 bg-green-500 hover:bg-green-600 transition duration-300"
+          >
+            {link.icon}
+          </a>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ButtonWhatsApp
+export default ButtonWhatsApp;
