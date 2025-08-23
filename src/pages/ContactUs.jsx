c008846ad169835a7e00bf69730f697b85c3422e
Rahul32-code <114091763+Rahul32-code@users.noreply.github.com>
2025-08-23 20:03:20 +0530

    contact fix

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -10,11 +10,11 @@ const ContactUs = () => {
     <section id="contact" className="scroll-m-20 bg-white/25 py-10">
       <div className="container mx-auto px-4 sm:px-6 lg:px-20 flex flex-col-reverse lg:flex-row items-center justify-between gap-5">
         <div className="w-full lg:w-1/2 space-y-4 lg:text-left">
-          <h2 className="text-2xl sm:text-3xl lg:text-5xl font-bold">
+          <h2 className="text-2xl sm:text-3xl lg:text-5xl font-bold pb-5">
             Contact <span className="text-green-600">Us</span>
           </h2>
-          <p className="text-slate-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit...
+          <p className="text-slate-600 pb-10">
+            when shared with the right people. Whether you're building your brand from scratch or scaling something great, we're all ears. Reach out, and let’s turn conversations into creations.
           </p>
           <button
             className="text-xl font-medium py-1 px-4 border-2 border-green-600 text-green-600 rounded-full hover:bg-green-700 hover:text-white transition-all duration-300 flex items-center justify-center mt-4 cursor-pointer"
@@ -27,7 +27,7 @@ const ContactUs = () => {
           <img
             src={contact}
             alt="contact"
-            className="sm:w-90 lg:w-auto"
+            className="sm:w-80 lg:w-auto"
           />
         </div>
       </div>
